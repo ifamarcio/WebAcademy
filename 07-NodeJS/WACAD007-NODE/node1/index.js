acb6831ffd5bb0f9663cfdda583fc2e067ff890e
Marcio Viana <marcioferreira_viana@hotmail.com>
2025-05-20 07:39:41 -0400

    atualização 2 index.js

--- 07-NodeJS/WACAD007-NODE/node1/index.js
+++ 07-NodeJS/WACAD007-NODE/node1/index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const { createLink } = require('./util');
 
 // Determina o ambiente: development ou production
 const env = process.env.NODE_ENV || 'development';
@@ -10,6 +11,17 @@ const dir = process.argv[2] || __dirname;
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
+  const filePath = path.join(dir, req.url);
+
+  // Se estiver acessando um arquivo (ex: /nodejs.txt)
+  if (fs.existsSync(filePath) && fs.statSync(filePath).isFile()) {
+    const content = fs.readFileSync(filePath, 'utf-8');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(`<a href="/">Voltar</a><pre>${content}</pre>`);
+    return;
+  }
+
+  // Senão, mostra os links
   fs.readdir(dir, (err, files) => {
     if (err) {
       res.writeHead(500, {'Content-Type': 'text/plain'});
@@ -18,11 +30,9 @@ const server = http.createServer((req, res) => {
     }
 
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<ul>');
     files.forEach(file => {
-      res.write(`<li>${file}</li>`);
+      res.write(createLink(file));
     });
-    res.write('</ul>');
     res.end();
   });
 });
