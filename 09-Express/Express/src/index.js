77c3aad17dc0d1edb83c18834a6a4534324ca3c8
Marcio Viana <marcioferreira_viana@hotmail.com>
2025-06-11 16:13:37 -0400

    Create index.js

---
+++ 09-Express/Express/src/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>');
+});
+
+app.listen(PORT, () => {
+  console.log(`Express app iniciada na porta ${PORT}`);
+});
