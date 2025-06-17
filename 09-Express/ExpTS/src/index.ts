af1fb91eac133c22acce73bda357c065e459b64e
Marcio Viana <marcioferreira_viana@hotmail.com>
2025-06-17 16:59:51 -0400

    Update index.ts

--- 09-Express/ExpTS/src/index.ts
+++ 09-Express/ExpTS/src/index.ts
@@ -1,28 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import validateEnv from './utils/validateEnv';
-import fs from 'fs';
-import path from 'path';
 import loggerMiddleware from './middleware/logger';
+import routes from './routes/index.js';
 
-// Carrega variáveis de ambiente
 dotenv.config();
 validateEnv();
 
 const app = express();
-const port = process.env.PORT || 3333;
+const port = Number(process.env.PORT) || 3333;
 const logType = process.env.LOG_TYPE || 'simples';
 const logDir = process.env.LOG_DIR || './logs';
 
 // Middleware de log
 app.use(loggerMiddleware(logType, logDir));
 
-// Rota simples
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>');
-});
+// Rotas externas
+app.use(routes);
 
-// Inicializa o servidor
 app.listen(port, () => {
   console.log(`Servidor iniciado na porta ${port}`);
 });
