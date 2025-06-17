f394f05436d2480295e0cfd9fabc78f956c97a14
Marcio Viana <marcioferreira_viana@hotmail.com>
2025-06-17 17:07:45 -0400

    Update index.ts

--- 09-Express/ExpTS/src/routes/index.ts
+++ 09-Express/ExpTS/src/routes/index.ts
@@ -9,25 +9,30 @@ routes.get('/', (req, res) => {
 
 routes.get('/lorem/:qtd', (req, res) => {
   const qtd = parseInt(req.params.qtd);
-
   if (isNaN(qtd) || qtd <= 0) {
-    return res.status(400).json({ error: 'Informe um número válido de parágrafos.' });
+    return res.status(400).json({ error: 'Informe um número válido.' });
   }
 
   const lorem = new LoremIpsum({
-    sentencesPerParagraph: {
-      max: 8,
-      min: 4
-    },
-    wordsPerSentence: {
-      max: 16,
-      min: 4
-    }
+    sentencesPerParagraph: { max: 8, min: 4 },
+    wordsPerSentence: { max: 16, min: 4 }
   });
 
-  const parags = lorem.generateParagraphs(qtd);
+  const texto = lorem.generateParagraphs(qtd);
+  res.send(`<pre>${texto}</pre>`);
+});
 
-  res.send(`<pre>${parags}</pre>`);
+// Rotas Handlebars
+routes.get('/hb1', (_req, res) => {
+  res.render('hb1');
+});
+
+routes.get('/hb2', (_req, res) => {
+  res.render('hb2');
+});
+
+routes.get('/hb3', (_req, res) => {
+  res.render('hb3');
 });
 
 export default routes;
