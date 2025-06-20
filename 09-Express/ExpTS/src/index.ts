630a815b5be7b0a18398f2ee370036672ea63163
Marcio Viana <marcioferreira_viana@hotmail.com>
2025-06-20 07:14:25 -0400

    Update index.ts

--- 09-Express/ExpTS/src/index.ts
+++ 09-Express/ExpTS/src/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import { engine } from 'express-handlebars';
+
 import validateEnv from './utils/validateEnv';
 import loggerMiddleware from './middleware/logger';
 import routes from './routes/index.js';
-import path from 'path';
 
 dotenv.config();
 validateEnv();
@@ -14,17 +15,28 @@ const port = Number(process.env.PORT) || 3333;
 const logType = process.env.LOG_TYPE || 'simples';
 const logDir = process.env.LOG_DIR || './logs';
 
-// View engine Handlebars
-app.engine('handlebars', engine());
+// 🧠 Configuração do Handlebars com helper personalizado
+app.engine(
+  'handlebars',
+  engine({
+    helpers: {
+      ifNode: function (value: boolean, options: any) {
+        return value ? options.fn(this) : options.inverse(this);
+      }
+    }
+  })
+);
 app.set('view engine', 'handlebars');
 app.set('views', path.resolve('src', 'views'));
 
-// Middleware de log
+// Middleware
 app.use(loggerMiddleware(logType, logDir));
 
 // Rotas
 app.use(routes);
 
+// Inicia servidor
 app.listen(port, () => {
   console.log(`Servidor iniciado na porta ${port}`);
 });
+
