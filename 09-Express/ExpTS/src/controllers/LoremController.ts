6e93bb6037c0f75e939267ef2393178db5273158
Marcio Viana <marcioferreira_viana@hotmail.com>
2025-06-25 10:54:51 -0400

    Update LoremController.ts

--- 09-Express/ExpTS/src/controllers/LoremController.ts
+++ 09-Express/ExpTS/src/controllers/LoremController.ts
@@ -1,11 +1,38 @@
-// src/controllers/LoremController.ts
 import { Request, Response } from 'express';
 import { LoremIpsum } from 'lorem-ipsum';
 
-const lorem = new LoremIpsum();
+export const home = (req: Request, res: Response) => {
+  res.send('Hello World!');
+};
 
-export const getLorem = (req: Request, res: Response) => {
-  const num = parseInt(req.params.num || '1');
-  const paragraphs = lorem.generateParagraphs(num);
-  res.send(`<p>${paragraphs.replace(/\n/g, '</p><p>')}</p>`);
+export const lorem = (req: Request, res: Response) => {
+  const paragraphs = parseInt(req.params.qtd) || 1;
+
+  const lorem = new LoremIpsum();
+  const result = Array(paragraphs).fill(null).map(() => `<p>${lorem.generateParagraphs(1)}</p>`).join('');
+
+  res.send(result);
+};
+
+export const hb1 = (req: Request, res: Response) => {
+  res.render('hb1', { title: 'Página HB1' });
+};
+
+export const hb2 = (req: Request, res: Response) => {
+  res.render('hb2', { name: 'Usuário', year: new Date().getFullYear() });
+};
+
+export const hb3 = (req: Request, res: Response) => {
+  const fruits = ['Maçã', 'Banana', 'Laranja'];
+  res.render('hb3', { fruits });
+};
+
+export const hb4 = (req: Request, res: Response) => {
+  const tecnologias = [
+    { name: 'Node.js', type: 'runtime', poweredByNodejs: true },
+    { name: 'Express', type: 'framework', poweredByNodejs: true },
+    { name: 'React', type: 'frontend', poweredByNodejs: false },
+  ];
+
+  res.render('hb4', { tecnologias });
 };
